c58442364616ca2ef3ee54237e77a56d28a6945f
dylan9527 <ghost_stars@126.com>
2017-03-01 11:55:47 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("Baby ");
+        sb.append("Good morning\n");
+        System.out.print(sb);
+        System.out.println("Hello World!");
+    }
+}
